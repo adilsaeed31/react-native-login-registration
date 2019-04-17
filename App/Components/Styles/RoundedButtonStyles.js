0e85bdf07c098ec60f82ab9d91f541db5098c40c
Muhammad Adil <adilsaeed31@gmail.com>
2019-04-17 18:37:19 +0400

    Initial

--- App/Components/Styles/RoundedButtonStyles.js
+++ App/Components/Styles/RoundedButtonStyles.js
@@ -1,20 +1,23 @@
-import { StyleSheet } from 'react-native'
-import { Fonts, Colors, Metrics } from '../../Themes/'
+import { StyleSheet } from 'react-native';
+import { Fonts, Colors, Metrics } from '../../Themes/';
 
 export default StyleSheet.create({
-  button: {
-    height: 45,
-    borderRadius: 5,
-    marginHorizontal: Metrics.section,
-    marginVertical: Metrics.baseMargin,
-    backgroundColor: Colors.fire,
-    justifyContent: 'center'
-  },
-  buttonText: {
-    color: Colors.snow,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: Fonts.size.medium,
-    marginVertical: Metrics.baseMargin
-  }
-})
+	button: {
+		width: 120,
+		height: 45,
+		borderRadius: 5,
+		marginHorizontal: Metrics.section,
+		marginVertical: Metrics.baseMargin,
+		backgroundColor: Colors.panther,
+		justifyContent: 'center',
+		paddingLeft: 20,
+		paddingRight: 20
+	},
+	buttonText: {
+		color: Colors.snow,
+		textAlign: 'center',
+		fontWeight: 'bold',
+		fontSize: Fonts.size.medium,
+		marginVertical: Metrics.baseMargin
+	}
+});
--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,32 +1,129 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
-
-import { Images } from '../Themes'
+import React, { Component } from 'react';
+import { Alert, Text, Image, View } from 'react-native';
+import { Container, Content, Form, Item, Input, Label } from 'native-base';
+import DevscreensButton from '../../ignite/DevScreens/DevscreensButton';
+import { Images } from '../Themes';
+import Reactotron from 'reactotron-react-native';
 
 // Styles
-import styles from './Styles/LaunchScreenStyles'
-
+import styles from './Styles/LaunchScreenStyles';
 export default class LaunchScreen extends Component {
-  render () {
-    return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
+	state = {
+		loginForm: true
+	};
+	renderForm = (loginForm) => {
+		if (loginForm) {
+			return (
+				<Form>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>E-mail</Label>
+						<Input
+							name="email"
+							onChangeText={(text) => this._setState('email', text)}
+							style={{ color: 'white' }}
+						/>
+					</Item>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>Password</Label>
+						<Input
+							name="password"
+							onChangeText={(text) => this._setState('password', text)}
+							style={{ color: 'white' }}
+							secureTextEntry={true}
+						/>
+					</Item>
+				</Form>
+			);
+		} else {
+			return (
+				<Form>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>Name</Label>
+						<Input onChangeText={(text) => this._setState('name', text)} style={{ color: 'white' }} />
+					</Item>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>E-mail</Label>
+						<Input onChangeText={(text) => this._setState('email', text)} style={{ color: 'white' }} />
+					</Item>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>Password</Label>
+						<Input onChangeText={(text) => this._setState('password', text)} style={{ color: 'white' }} />
+					</Item>
+					<Item floatingLabel>
+						<Label style={{ color: 'white' }}>Confirm Password</Label>
+						<Input
+							onChangeText={(text) => this._setState('confirmPassword', text)}
+							style={{ color: 'white' }}
+							secureTextEntry={true}
+						/>
+					</Item>
+				</Form>
+			);
+		}
+	};
 
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
+	handleSubmit = () => {
+		// Reactotron.log(this.state.email, 'state');
 
-          <DevscreensButton />
-        </ScrollView>
-      </View>
-    )
-  }
+		fetch('https://api-ordertogether.herokuapp.com/auth/signin', {
+			method: 'POST',
+			body: JSON.stringify({ email: this.state.email, password: this.state.password }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(async (res) => {
+			let { message, status } = await res;
+			if (status === 200) {
+				Alert.alert('Success', message);
+			} else {
+				Alert.alert('Error', 'Something went wrong!');
+			}
+		});
+	};
+
+	changeForm = () => {
+		this.setState({ loginForm: !this.state.loginForm });
+	};
+
+	_setState = (name, value) => {
+		this.setState({ [name]: value });
+	};
+
+	render() {
+		let { loginForm } = this.state;
+		return (
+			<Container style={{ backgroundColor: 'rgba(0,0,0,.85)' }}>
+				<Content>
+					<View style={[ styles.centered, { marginTop: 30 } ]}>
+						<Text style={styles.text}>#OrderTogether</Text>
+					</View>
+					<View
+						style={{
+							flex: 1,
+							margin: 20,
+							marginRight: 50
+						}}
+					>
+						{this.renderForm(loginForm)}
+						<View
+							style={[
+								styles.centered,
+								{ flex: 1, flexDirection: 'row', justifyContent: 'center', marginTop: 30 }
+							]}
+						>
+							<View>
+								<DevscreensButton name={'Submit'} onPress={this.handleSubmit} />
+							</View>
+							<View>
+								<DevscreensButton name={loginForm ? 'Register' : 'Login'} onPress={this.changeForm} />
+							</View>
+						</View>
+						<View style={[ styles.centered, { marginTop: 30 } ]}>
+							<Image source={Images.ready} />
+						</View>
+					</View>
+				</Content>
+			</Container>
+		);
+	}
 }
